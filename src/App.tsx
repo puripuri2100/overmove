e85750547255d98c3031e2482036f867f51b22a3
puripuri2100 <puripuri2100@gmail.com>
2024-11-02 21:18:50 +0900

    データの保存とマップへのデータ表示を実装

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,23 @@ import {
   writeTextFile,
   BaseDirectory,
   exists,
-  create
+  create,
 } from '@tauri-apps/plugin-fs';
 import Switch from "react-switch";
 import { CopyBlock, github } from "react-code-blocks";
+import {
+  format,
+  differenceInSeconds
+} from "date-fns";
 import L, {Map} from "leaflet";
-import { MapContainer, TileLayer, useMap, Polyline, Circle } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Polyline,
+  Circle,
+  Popup
+} from "react-leaflet";
 import "leaflet.offline";
 import "leaflet/dist/leaflet.css";
 import "./App.css";
@@ -67,7 +78,11 @@ type allDataType = {
 // 地図に表示する移動の情報
 type mapMoveInfo = {
   moveInfo: move,
-  geolocationList: geolocation[]
+  geolocationList: geolocation[],
+  maxSpeed: number | null,
+  averageSpeed: number | null,
+  distanceSum: number,
+  moveSeconds: number,
 }
 
 // モード選択
@@ -101,35 +116,45 @@ function App() {
 
   useEffect(() => {
     (async() => {
-      const existsTravelListFile = await exists(travelListFilePath, {baseDir: BaseDirectory.AppData});
+      const existsTravelListFile = await exists(travelListFilePath, {baseDir: BaseDirectory.AppLocalData});
       if (existsTravelListFile) {
-        const travelListFileText = await readTextFile(travelListFilePath, {baseDir: BaseDirectory.AppData});
-        info(`travelListFileText: ${travelListFileText}`);
+        info(`exists: travelListFilePath`);
+        const travelListFileText = await readTextFile(travelListFilePath, {baseDir: BaseDirectory.AppLocalData});
         setTravelList(JSON.parse(travelListFileText));
         info("read travelList");
       } else {
-        create(travelListFilePath, {baseDir: BaseDirectory.AppData});
+        info("not exists: travelListFilePath");
+        const file = await create(travelListFilePath, {baseDir: BaseDirectory.AppLocalData});
+        await file.write(new TextEncoder().encode("[]"));
+        await file.close();
         info("create travelList");
       }
 
-      const existsMoveListFile = await exists(moveListFilePath, {baseDir: BaseDirectory.AppData});
+      const existsMoveListFile = await exists(moveListFilePath, {baseDir: BaseDirectory.AppLocalData});
       if (existsMoveListFile) {
-        const moveListFileText = await readTextFile(moveListFilePath, {baseDir: BaseDirectory.AppData});
-        info(`moveListFileText: ${moveListFileText}`);
+        info(`exists: moveListFilePath`);
+        const moveListFileText = await readTextFile(moveListFilePath, {baseDir: BaseDirectory.AppLocalData});
         setMoveList(JSON.parse(moveListFileText));
         info("read moveList");
       } else {
-        create(moveListFilePath, {baseDir: BaseDirectory.AppData});
+        info(`not exists: moveListFilePath`);
+        const file = await create(moveListFilePath, {baseDir: BaseDirectory.AppLocalData});
+        await file.write(new TextEncoder().encode("[]"));
+        await file.close();
         info("create moveList");
       }
 
-      const existsGeolocationListFile = await exists(geolocationListFilePath, {baseDir: BaseDirectory.AppData});
+      const existsGeolocationListFile = await exists(geolocationListFilePath, {baseDir: BaseDirectory.AppLocalData});
       if (existsGeolocationListFile) {
-        const geolocationListFileText = await readTextFile(geolocationListFilePath, {baseDir: BaseDirectory.AppData});
-        info(`geolocationListFileText: ${geolocationListFileText}`);
+        info(`exists: geolocationListFilePath`);
+        const geolocationListFileText = await readTextFile(geolocationListFilePath, {baseDir: BaseDirectory.AppLocalData});
         setGeolocationList(JSON.parse(geolocationListFileText));
       } else {
-        create(geolocationListFilePath, {baseDir: BaseDirectory.AppData});
+        info(`not exists: geolocationListFilePath`);
+        const file = await create(geolocationListFilePath, {baseDir: BaseDirectory.AppLocalData});
+        await file.write(new TextEncoder().encode("[]"));
+        await file.close();
+        info("create geolocationList");
       }
     })();
   }, [])
@@ -137,23 +162,43 @@ function App() {
 
   useEffect(() => {
     (async() => {
-      info(`travelList: ${travelList}`);
-      await writeTextFile(travelListFilePath, JSON.stringify(travelList), {baseDir: BaseDirectory.AppData});
-      info("write travelList");
+      const isTravelListFileOnAppLocalDataDir = await exists(travelListFilePath, {baseDir: BaseDirectory.AppLocalData});
+      info(`isTravelListFileOnAppLocalDataDir: ${isTravelListFileOnAppLocalDataDir}`);
+
+      const nowText = await readTextFile(travelListFilePath, {baseDir: BaseDirectory.AppLocalData});
+      const data: travel[] = JSON.parse(nowText);
+      if (data.length < travelList.length) {
+        await writeTextFile(travelListFilePath, JSON.stringify(travelList), {baseDir: BaseDirectory.AppLocalData});
+        info("write travelList");
+      }
     })()
   }, [travelList]);
 
   useEffect(() => {
     (async() => {
-      await writeTextFile(moveListFilePath, JSON.stringify(moveList), {baseDir: BaseDirectory.AppData});
-      info("write moveList");
+      const isTravelListFileOnAppLocalDataDir = await exists(travelListFilePath, {baseDir: BaseDirectory.AppLocalData});
+      info(`isTravelListFileOnAppLocalDataDir: ${isTravelListFileOnAppLocalDataDir}`);
+
+      const nowText = await readTextFile(moveListFilePath, {baseDir: BaseDirectory.AppLocalData});
+      const data: move[] = JSON.parse(nowText);
+      if (data.length < moveList.length) {
+        await writeTextFile(moveListFilePath, JSON.stringify(moveList), {baseDir: BaseDirectory.AppLocalData});
+        info("write moveList");
+      }
     })()
   }, [moveList]);
 
   useEffect(() => {
     (async() => {
-      await writeTextFile(geolocationListFilePath, JSON.stringify(geolocationList), {baseDir: BaseDirectory.AppData});
-      info("write geolocationList");
+      const isTravelListFileOnAppLocalDataDir = await exists(travelListFilePath, {baseDir: BaseDirectory.AppLocalData});
+      info(`isTravelListFileOnAppLocalDataDir: ${isTravelListFileOnAppLocalDataDir}`);
+
+      const nowText = await readTextFile(geolocationListFilePath, {baseDir: BaseDirectory.AppLocalData});
+      const data: move[] = JSON.parse(nowText);
+      if (data.length < geolocationList.length) {
+        await writeTextFile(geolocationListFilePath, JSON.stringify(geolocationList), {baseDir: BaseDirectory.AppLocalData});
+        info("write geolocationList");
+      }
     })()
   }, [geolocationList]);
 
@@ -263,17 +308,88 @@ function App() {
 
   const [isSetMapCenter, setIsSetMapCenter] = useState<boolean>(false);
 
+  // 現在位置に移動速度などを表示するかどうかのフラグ
+  const [isNowPosPopup, setIsNowPosPopup] = useState(false);
+
+  // m/s -> km/h
+  function convertSpeed(speed: number): number {
+    return speed * 3.6
+  }
+
+  // 方角の値から向きを出す関数
+  function headingValueToDirection(heading: number): string {
+    if ((0 <= heading && heading < 22.5) || 337.5 <= heading) {
+      return "北"
+    } else if (22.5 <= heading && heading < 67.5) {
+      return "北東"
+    } else if (67.5 <= heading && heading < 112.5) {
+      return "東"
+    } else if (112.5 <= heading && heading < 157.5) {
+      return "南東"
+    } else if (157.5 <= heading && heading < 202.5) {
+      return "南"
+    } else if (202.5 <= heading && heading < 247.5) {
+      return "南西"
+    } else if (247.5 <= heading && heading < 292.5) {
+      return "西"
+    } else if (292.5 <= heading && heading < 337.5) {
+      return "北西"
+    } else {
+      return ""
+    }
+  }
+
+  // 2地点間の緯度経度から間の距離を求める
+  // Haversineの公式を用いる
+  // 赤道半径R: 6378.137km
+  // 緯度x1, 経度y1
+  // 緯度y1, 経度y2
+  // D = R * acos(sin(y1) * sin(y2) + cos(y1) * cos(y2) * cos(x2 - x1))
+  function calDistance(x1: number, y1: number, x2: number, y2: number): number {
+    const r = 6378.137;
+    return r * Math.acos(Math.sin(y1) * Math.sin(y2) + Math.cos(y1) * Math.cos(y2) * Math.cos(x2 - x1));
+  }
 
   // 選択された旅行に含まれる移動のリストを作成する
   useEffect(() => {
-    const travelInfo = travelList.find((value) => value.id == showMapId);
+    const targetTravelId = showMapId == "here" ? nowTravelId : showMapId;
+    const travelInfo = travelList.find((value) => value.id == targetTravelId);
     if (travelInfo) {
-      const targetMoveList = moveList.filter((value) => {travelInfo.move_id_list.includes(value.id)});
+      const targetMoveList = moveList.filter((value) => travelInfo.move_id_list.includes(value.id));
       const targetGeoList = targetMoveList.map((moveInfo) => {
-        const lst = geolocationList.filter((geo) => {moveInfo.start <= geo.timestamp && geo.timestamp <= moveInfo.end});
+        const lst = geolocationList.filter((geo) => moveInfo.start <= geo.timestamp && geo.timestamp <= moveInfo.end);
+        let maxSpeed: number | null = null;
+        let tempX = 0;
+        let tempY = 0;
+        let distanceSum = 0;
+        for (let i = 0; i < lst.length; i++) {
+          const value = lst[i];
+          if (value.speed) {
+            if (maxSpeed) {
+              if (maxSpeed < value.speed) {
+                maxSpeed = value.speed;
+              }
+            } else {
+              maxSpeed = value.speed
+            }
+          }
+
+          if (i != 0) {
+            const x = value.latitude;
+            const y = value.longitude;
+            const d = calDistance(x, y, tempX, tempY);
+            distanceSum += d;
+            tempX = x;
+            tempY = y;
+          }
+        }
         const data: mapMoveInfo = {
           moveInfo,
-          geolocationList: lst
+          geolocationList: lst,
+          maxSpeed,
+          averageSpeed: null,
+          distanceSum,
+          moveSeconds: differenceInSeconds(moveInfo.end, moveInfo.start)
         };
         return data
       });
@@ -283,6 +399,10 @@ function App() {
     }
   }, [showMapId]);
 
+  // 情報を見たい移動のIDとクリックした位置
+  const [showMoveInfo, setShowMoveInfo] = useState<mapMoveInfo | null>(null);
+  const [showMoveInfoClickPosX, setShowMoveInfoClickPosX] = useState(0);
+  const [showMoveInfoClickPosY, setShowMoveInfoClickPosY] = useState(0);
 
   useEffect(() => {
     if(map){
@@ -459,15 +579,47 @@ function App() {
                       weight: 10
                     }}
                     positions={moveInfo.geolocationList.map((value) => [value.latitude, value.longitude])}
+                    eventHandlers={{
+                      click: (event) => {
+                        setShowMoveInfo(moveInfo);
+                        setShowMoveInfoClickPosX(event.latlng.lat);
+                        setShowMoveInfoClickPosY(event.latlng.lng);
+                      }
+                    }}
                   />
                 })
               }
-              {(nowGeolocation) ?
+              {
+                showMoveInfo ?
+                  <Popup position={[showMoveInfoClickPosX, showMoveInfoClickPosY]}>
+                    <>{showMoveInfo.moveInfo.start.toLocaleString()} から {showMoveInfo.moveInfo.end}まで<br /></>
+                    <>
+                      移動時間：
+                      {Math.round(showMoveInfo.moveSeconds / 3600)}時間
+                      {Math.round(showMoveInfo.moveSeconds / 60) % 60}分
+                      {(showMoveInfo.moveSeconds % 60)}秒<br />
+                    </>
+                    <>移動距離：{Math.round(showMoveInfo.distanceSum / 100) / 10}km<br /></>
+                    <>平均速度：{Math.round((showMoveInfo.distanceSum / (showMoveInfo.moveSeconds / 3600)) / 100) / 10}km/h</>
+                  </Popup>
+                : null
+              }
+              {nowGeolocation ?
                 <Circle
                   center={[nowGeolocation.latitude, nowGeolocation.longitude]}
                   pathOptions={{fillColor: "blue"}}
                   radius={40}
+                  eventHandlers={{
+                    click: () => {setIsNowPosPopup(!isNowPosPopup)}
+                  }}
                 />
+              : null}
+              {nowGeolocation && isNowPosPopup ?
+                <Popup position={[nowGeolocation.latitude, nowGeolocation.longitude]}>
+                  {nowGeolocation.speed ? <>速度：{convertSpeed(nowGeolocation.speed)}km/h<br/></> : null}
+                  {nowGeolocation.altitude ? <>高度：{nowGeolocation.altitude}<br/></> : null}
+                  {nowGeolocation.heading ? <>方角：{headingValueToDirection(nowGeolocation.heading)}</> : null}
+                </Popup>
               : null}
             </MapContainer>
           </>
